7c6daedb06b99ef90e7a754a4a84253dd87a342b
cvvnx1 <cvvnx1@hotmail.com>
2020-12-18 13:05:35 +0800

    update grok_exporter deploy

--- grok_exporter/deploy.sh
+++ grok_exporter/deploy.sh
@@ -1,15 +1,17 @@
 # shellcheck disable=SC2164
-PWD="$(pwd)"
+PWD="$( pwd -P )"
 ROOT="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd ${ROOT}
 
 for tag in *; do
+  echo "Scan {tag} ..."
   [[ -d "${tag}" ]] \
   && echo "Push ${tag} ..." \
   && cd ${ROOT}/${tag} \
   && docker build . -t cvvnx1/grok_exporter:${tag} \
-  && docker push cvvnx1/grok_exporter:${tag}
+  && docker push cvvnx1/grok_exporter:${tag} \
+  && cd ${ROOT}
 done
 
 cd ${PWD}
