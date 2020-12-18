49dcfaf04ec70a618e9ce2b50b1e8c0d2596adfc
cvvnx1 <cvvnx1@hotmail.com>
2020-12-18 12:51:22 +0800

    update grok_exporter deploy

--- grok_exporter/deploy.sh
+++ grok_exporter/deploy.sh
@@ -3,8 +3,8 @@ ROOT="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 for f in ${ROOT}/*; do
   tag="$(basename ${f})"
-  echo "Push ${tag} ..."
   [[ -d "${tag}" ]] \
+  echo "Push ${tag} ..." \
   && cd ${ROOT}/${tag} \
   && docker build . -t cvvnx1/grok_exporter:${tag} \
   && docker push cvvnx1/grok_exporter:${tag}
